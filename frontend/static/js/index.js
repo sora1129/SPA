616ee62cef41d426ba7d7b0f6a6d6d84d25182f1
sora1129 <64789798+sora1129@users.noreply.github.com>
2022-03-18 21:26:09 +0900

    URLの調整

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -2,15 +2,29 @@ import Dashboard from "./views/Dashboard.js";
 import Posts from "./views/Posts.js";
 import Settings from "./views/Settings.js";
 
+const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
+const getParams = match => {
+	const values = match.result.slice(1);
+	const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+	console.log(Array.from(match.route.path.matchAll(/:(\w+)/g)));
+
+	return {};
+};
+
 const navigateTo = url => {
 	history.pushState(null, null, url);
 	router();
 }
 
 const router = async () => {
+	console.log(pathToRegex("/posts/:id"));
+	// posts/:id
 	const routes = [
 		{ path: "/", view: Dashboard },
 		{ path: "/posts", view: Posts },
+		{ path: "/posts/:id", view: Posts },
 		{ path: "/settings", view: Settings },
 	];
 
@@ -18,11 +32,11 @@ const router = async () => {
 	const potentialMatches = routes.map(route => {
 		return {
 			route: route,
-			isMatch: location.pathname === route.path
+			result: location.pathname.match(pathToRegex(route.path))
 		};
 	});
 
-	let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+	let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
 
 	if (!match) {
 		match = {
@@ -31,11 +45,10 @@ const router = async () => {
 		};
 	}
 
-	const view = new match.route.view();
+	const view = new match.route.view(getParams(match));
 
 	document.querySelector("#app").innerHTML = await view.getHtml();
 
-	console.log(match.route.view());
 };
 
 window.addEventListener("popstate", router);
