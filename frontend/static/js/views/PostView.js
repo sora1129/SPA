367a3907dd25b3cd9e98c0c75323500502c7ab82
sora1129 <64789798+sora1129@users.noreply.github.com>
2022-03-19 15:42:26 +0900

    post idの設定

--- frontend/static/js/views/PostView.js
+++ frontend/static/js/views/PostView.js
@@ -3,14 +3,14 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
 	constructor(params) {
 		super(params);
+		this.postId = params.id;
 		this.setTitle("Viewing Post");
 	}
 
 	async getHtml() {
-		console.log(this.params.id);
 		return `
 		<h1>Posts</h1>
-		<p>You are viewing the posts!</p>
+		<p>You are viewing the posts #${this.postId}.</p>
 		`;
 	}
 }
